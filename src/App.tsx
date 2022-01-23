c67db322743d67c473f25e2048ebf14d549bcb27
raraku <airenov500@gmail.com>
2022-01-23 19:22:38 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,26 @@ function App() {
     console.log(video);
 
     // request access to webcam
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function (constraints) {
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia =
+          navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+          return Promise.reject(
+            new Error("getUserMedia is not implemented in this browser")
+          );
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      };
+    }
     navigator.mediaDevices
       .getUserMedia({ video: { width: 426, height: 240 } })
       .then((stream) => (video.srcObject = stream));
